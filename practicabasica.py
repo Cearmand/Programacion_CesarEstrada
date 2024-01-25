4dae81434532e61b3c5eaec2b51e2356d402da37
Cearmand <105522798+Cearmand@users.noreply.github.com>
2024-01-25 11:35:47 -0600

    Add files via upload

---
+++ practicabasica.py
@@ -0,0 +1,39 @@
+import math
+##Calcular grados celsius a farenhei
+# print("CONVERTIR DE CELSIUS A Fahrenheit")
+# celciu=float(input("Ingrese los grados celcius: "))
+# faren=(celciu*(9/5))+32
+# print("Los grados Fahrenheit son: ",faren)
+
+##Un programa que calcule la hora, indique cuantos segundos han trascurrido del dia
+# print("CALCULAR LOS SEGUNDOS TRASNCURRIDOS DEL DIA")
+# hora=float(input("Ingrese la hora actual "))
+# segundos=hora*3600
+# print("Los segundos transcurridos son: ",segundos)
+
+##Un programa que convierta divisas (dolares-quetzaes)
+# print("CAMBIO DE DOLAR A QUETZAL")
+# print("Ingrese el cambio que desa hacer: \n1. Dolar\n2. Euro\n3. Yen")
+# tipo=int(input("Ingrese la opcion: "))
+# cantidad=float(input("Ingrese la cantidad que desea convertit: Q"))
+# if tipo==1:
+#     resultado=cantidad*0.13
+#     print("Dolar: ",resultado)
+# elif tipo==2:
+#     resultado=cantidad*0.12
+#     print("Euro: ",resultado)
+# elif tipo==3:
+#     resultado=cantidad*18.85
+#     print("Yen: ",resultado)
+
+##Opere la forma cuadratica
+# print("Calculo de la ecuacion cuadratica")
+# a=float(input("Ingrese la variabe a: "))
+# b=float(input("Ingrese la variabe b: "))
+# c=float(input("Ingrese la variabe c: "))
+# x1=(-b+math.sqrt((pow(b,2))-4*a*c))/(2*a)
+# x2=(-b-math.sqrt((pow(b,2))-4*a*c))/(2*a)
+# print("x1: ",x1,"\nx2: ",x2)
+
+##Indicar en que tipo de dato se deve ingresar esa variable
+
