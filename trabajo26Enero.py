b947ab29899ea8bf1c51f9156cd1df8e8f8055dd
Cearmand <105522798+Cearmand@users.noreply.github.com>
2024-01-26 11:37:08 -0600

    Add files via upload

---
+++ trabajo26Enero.py
@@ -0,0 +1,21 @@
+import math
+#Escribir un programa que muestre la cadena !Hola mundo¡
+print("!Hola mundo¡")
+
+#una variable que almacena la cadena hola mundo y se muestre en pantalla
+cadena="hola mundo"
+print(cadena.capitalize())
+
+#Leer el nombre del usuario en consola y luego imprimirlo
+user=input("Ingrese su nombre: ")
+print("!Hola",user+"¡")
+
+#(3+2/2.5)**2
+result=pow((3+2)/(2*5),2)
+print(result)
+
+#numero de horas trabajadas
+horas=float(input("Ingrese el numero de horas trabajadas: "))
+costo=float(input("Ingrese el costo por hora trabajada: "))
+resultado=costo*horas
+print(f"El salario total es: {resultado}")
