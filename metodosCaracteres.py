a244cb30d1afbbb4eac936f39973466139f22cac
Cearmand <105522798+Cearmand@users.noreply.github.com>
2024-01-27 15:15:47 -0600

    Add files via upload

---
+++ metodosCaracteres.py
@@ -0,0 +1,39 @@
+#capitalize para mayusculas
+texto= "holra mundo"
+resultado = texto.capitalize()
+print(resultado)
+
+#metodo find
+texto= "Hola mundo"
+resultado=texto.find("undo")
+# print(resultado)
+
+#Metodo center(Centra el texto en un conjunto de carateres)
+texto="Hola"
+resultado=texto.center(20,'*')
+# print(resultado)
+
+#metodo isalnum devuelve true si todo los caracteres de  cadena son alfanumericos, si se ponde .,*+}{-} devuelve un false
+texto="Python1234567"
+resultado=texto.isalnum()
+# print(resultado)
+
+#metodo isdigit devuelve    true si los caracteres de la cadena son digitos
+texto="123456"
+resultado=texto.isdigit()
+#print(resultado)
+
+#metodo islower, devuelve un tru si solo hay minusculas
+texto="asdFasdas"
+resultado=texto.islower()
+#print(resultado)
+
+#metodo isspace, devuelve true si todos los caractere de la cadena son espacios en blanco
+texto="  "
+resultado=texto.isspace()
+#print(resultado)
+
+#metodo istrip
+#replace 
+#rstrip
+#split
